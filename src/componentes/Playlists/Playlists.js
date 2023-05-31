bcad5f6c4bd9ccf1401c24c30f84fecb3766c9a8
henriquedias420 <henriquediasper@hotmail.com>
2023-05-31 10:57:41 -0300

    finalizei o exercicio de integracao com apis 1

--- src/componentes/Playlists/Playlists.js
+++ src/componentes/Playlists/Playlists.js
@@ -1,35 +1,65 @@
-import React, {  useState } from "react";
+import React, { useEffect, useState } from "react";
 import Musicas from "../Musicas/Musicas";
+import axios from "axios";
 
-const playlistsLocal = [
-    {
-        id: 1,
-        name: "Playlist 1"
-    },
-    {
-        id: 2,
-        name: "Playlist 2"
-    },
-    {
-        id: 3,
-        name: "Playlist 3"
-    },
-    {
-        id: 4,
-        name: "Playlist 4"
-    },
-]
+// const playlistsLocal = [
+//     {
+//         id: 1,
+//         name: "Playlist 1"
+//     },
+//     {
+//         id: 2,
+//         name: "Playlist 2"
+//     },
+//     {
+//         id: 3,
+//         name: "Playlist 3"
+//     },
+//     {
+//         id: 4,
+//         name: "Playlist 4"
+//     },
+// ]
+
 function Playlists() {
-    const [playlists, setPlaylists] = useState(playlistsLocal)
-  
-    return (
-        <div>
-            {playlists.map((playlist) => {
-                return <Musicas key={playlist.id} playlist={playlist}/>
-            })}
+  const [playlists, setPlaylists] = useState([]);
 
-        </div>
-    );
+  useEffect(() => {
+    pegaPlaylist();
+  }, []);
+
+  const pegaPlaylist = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists",
+        {
+          headers: {
+            Authorization: "henriquedias-faruqi",
+          },
+        }
+      )
+
+      .then((resposta) => {
+        setPlaylists(resposta.data.result.list);
+      })
+      .catch((erro) => {
+        console.log(erro.response);
+      });
+  };
+
+  return (
+    <div>
+      {playlists.map((playlist) => {
+        return (
+          <Musicas
+            key={playlist.id}
+            playlist={playlist}
+            pegaPlaylist={pegaPlaylist}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Playlists;
